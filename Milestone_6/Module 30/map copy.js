43c0858f06359a011fc6be4cd6b4c4e391fc7257
danialcodes <md.danial.islam@g.bracu.ac.bd>
2021-08-25 04:20:19 +0600

    Finish Module 31

---
+++ Milestone_6/Module 30/map copy.js
@@ -0,0 +1,17 @@
+const books = [
+    { name: 'a', price: 100 },
+    { name: 'b', price: 40 },
+    { name: 'c', price: 15 },
+    { name: 'd', price: 11 },
+    { name: 'e', price: 10 },
+    { name: 'f', price: 1 }
+];
+let ma = books.find(e => e.price==1);
+console.log(ma.name);
+console.log(books);
+
+let a = 0
+const total = books.map(p => p.price).map(t => {
+    a += t;
+    return a;
+});

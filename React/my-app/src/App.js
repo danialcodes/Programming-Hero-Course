b4ebb4f8be21f4c41e26c9bfdc711a28e604ad21
danialcodes <md.danial.islam@g.bracu.ac.bd>
2021-09-26 03:13:35 +0600

    Module 46 done

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter>
+      <ExternalUsers></ExternalUsers> */}
+      <LoadComments></LoadComments>
     </div>
   );
 }
+function LoadComments() {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(data => setComments(data));
+  }, []);
+  return (
+    <div id='commentBox'>
+      {
+        comments.map(comment => (<ShowComments id={comment.id} name={comment.name} email={comment.email} body={comment.body}></ShowComments>))
+      }
+    </div>
+  );
 
+}
+function ShowComments(props) {
+  const [id,setId] = useState(props.id);
+  const increaseId = () => setId(id+1);
+  const decreaseId =  () => setId(id-1);
+  return (
+     <div id='comment'>
+       <h1>Name: {props.name}</h1>
+       <h3>Id: {id}</h3>
+       <h4>Email: {props.email}</h4>
+       <p>{props.body}</p>
+       <button onClick={increaseId}>Increase ID</button>
+       <button onClick={decreaseId}>Decrease ID</button>
+     </div>
+  );
+}
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+  return (
+    <div >
+      <h1>External Users</h1>
+      {
+        users.map(user => (<User name={user.name} email={user.email}>
+        </User>))
+      }
+    </div>
+  );
+
+}
+function User(props) {
+  return (
+    <div className='Person'>
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.email}</p>
+    </div>
+  );
+}
+function Counter(props) {
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  );
+}
+{/* <Person name='Danial Islam' study="Brac University"></Person>
+      <Person name='Nishat Tasnim' study="Brac University"></Person>
+      <Person name='Danial Islam' study="Brac University"></Person> */}
+function Person(props) {
+  const person = {
+    border: '2px solid red',
+    backgroundColor: "rgb(211, 28, 7)",
+    margin: "10px",
+    borderRadius: "20px"
+  };
+  return (
+    <div className="Person" style={person}>
+      <h1>Name: {props.name}</h1>
+      <h4>Study: {props.study}</h4>
+    </div>
+  );
+}
 export default App;
